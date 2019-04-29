f83ac0930144a678d52697f0b693f87ff3a83853
David Schatz <david.schatz@tu-ilmenau.de>
2019-04-29 14:58:58 +0200

    Make widget configurable
    * Use "object orientated approach" instead of creating the two widgets
      right away with the call to require(...)
    * After require, one new "mail object" can be created, containing both
      the icon and count widget
    * Widgets may be configured during object creation with an args dict
    * Added support for some simple parameters already (most importantly
      the commands to run for counting/displaying unread mail)
    * TODO: change README accordingly

--- email-widget/email.lua
+++ email-widget/email.lua
@@ -5,38 +5,53 @@ local watch = require("awful.widget.watch")
 
 local path_to_icons = "/usr/share/icons/Arc/actions/22/"
 
-email_widget = wibox.widget.textbox()
-email_widget:set_font('Play 9')
+local email_widget = {}
 
-email_icon = wibox.widget.imagebox()
-email_icon:set_image(path_to_icons .. "/mail-mark-new.png")
+function email_widget:new(args)
+    self = {}
+    self.icon = wibox.widget.imagebox()
+    self.icon:set_image(path_to_icons .. "/mail-mark-new.png")
+    self.count = wibox.widget.textbox("")
 
-watch(
-    "python /home/<username>/.config/awesome/email-widget/count_unread_emails.py", 20,
+    if args.font then
+        self.count:set_font(args.font)
+    end
+
+    local count_cmd = args.count_cmd or [[echo "-"]]
+    local read_cmd = args.read_cmd or [[echo "Command for reading not specified"]]
+
+    watch(
+    count_cmd, args.interval or 20,
     function(widget, stdout, stderr, exitreason, exitcode)
         local unread_emails_num = tonumber(stdout) or 0
         if (unread_emails_num > 0) then
-        	email_icon:set_image(path_to_icons .. "/mail-mark-unread.png")
-	        email_widget:set_text(stdout)
+            self.icon:set_image(path_to_icons .. "/mail-mark-unread.png")
+            self.count:set_text(stdout)
         elseif (unread_emails_num == 0) then
-        	email_icon:set_image(path_to_icons .. "/mail-message-new.png")
-   	        email_widget:set_text("")
-        end	
+            self.icon:set_image(path_to_icons .. "/mail-message-new.png")
+            self.count:set_text("")
+        end
     end
-)
+    )
 
-
-function show_emails()
-    awful.spawn.easy_async([[bash -c 'python /home/<username>/.config/awesome/email-widget/read_unread_emails.py']],
-        function(stdout, stderr, reason, exit_code)   
+    function show_emails()
+        awful.spawn.easy_async(read_cmd,
+        function(stdout, stderr, reason, exit_code)
             naughty.notify{
                 text = stdout,
                 title = "Unread Emails",
                 timeout = 5, hover_timeout = 0.5,
-                width = 400,
+                width = args.display_width or 400,
             }
         end
-    )
+        )
+    end
+
+    self.icon:connect_signal("mouse::enter", function() show_emails() end)
+
+    return self
 end
 
-email_icon:connect_signal("mouse::enter", function() show_emails() end)
+return setmetatable(email_widget, {
+    __call = email_widget.new,
+})
